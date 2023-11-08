c1c5156e040a9642da403b25977876644288a275
TNTSuperMan <tntsuperga@gmail.com>
2023-11-08 20:11:10 +0900

    すすめた

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.NetworkInformation;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 internal partial class Program
 {
@@ -51,19 +50,26 @@ internal partial class Program
         if (VerifyDir(sc.sd + "\\config")) return;
         if (VerifyDir(sc.sd + "\\page")) return;
         if (VerifyDir(sc.sd + "\\resources")) return;
-        if (VerifyDir(sc.od)) return;
+        if (VerifyDir(sc.od)) mkdir(sc.od);
         if (VerifyFile(sc.sd + "\\config\\temp.json")) return;
         if (VerifyFile(sc.sd + "\\.advance\\index.html")) return;
         if (VerifyFile(sc.sd + "\\page\\footer.page")) return;
         if (VerifyFile(sc.sd + "\\page\\header.page")) return;
-        if (!sc.tisurl) if (VerifyFile(sc.textopage)) return;
-        if (!sc.lisurl) if (VerifyFile(sc.launcher)) return;
+        if (VerifyFile(sc.textopage)) return;
+        if (VerifyFile(sc.launcher)) return;
         log("--------------完了--------------");
         #endregion
 
         #region CopyFiles
         log("--------ファイルのコピー--------");
+        mkdir(sc.od + "\\script");
+        mkdir(sc.od + "\\config");
         cpdir(sc.sd + "\\page", sc.od + "\\page");
+        cpdir(sc.sd + "\\resources", sc.od + "\\resources");
+        cp(sc.textopage,sc.od + "\\script\\textopage.js");
+        cp(sc.launcher, sc.od + "\\script\\launcher.js");
+        cp(sc.sd + "\\.advance\\index.html", sc.od + "\\index.html");
+        cp(sc.sd + "\\config\\temp.json", sc.od + "\\config\\temp.json");
         log("--------------完了--------------");
         #endregion
     }
@@ -71,10 +77,8 @@ internal partial class Program
 public class SiteConfig
 {
     public string textopage;
-    public bool tisurl; //true:URL false:File
 
     public string launcher;
-    public bool lisurl;
 
     public string sd; //No last slash
 
